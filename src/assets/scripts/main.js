4fc51e282f797e75864784bc6e108db7d6d45f8f
Zakk <zakk@nclud.com>
2016-08-21 09:31:25 -0400

    all the things

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -5,35 +5,43 @@ document.onreadystatechange = function() {
   if ( document.readyState === 'complete' ) {
     console.log('ready');
 
-    var image = document.getElementById('intro-image');
-    var title = document.getElementById('intro-title');
+    var image = document.getElementById('intro-image'),
+        title = document.getElementById('intro-title'),
+        subtitle = document.getElementById('intro-subtitle'),
+        more = document.getElementById('intro-more');
 
     var timer = window.setTimeout(function() {
+      var tl = new TimelineLite();
 
-      TweenMax.to(image, 2, {
+      // animate image in
+      tl.to(image, 1, {
         opacity: 0.4,
         scale: 0.95,
-        ease: Power4.easeOut
+        ease: Power4.easeIn
       })
 
+      // animate title in
       var mySplitText = new SplitText( title, { type: 'chars' } ),
           chars = mySplitText.chars
 
-      console.log(chars);
-
-
-      TweenMax.set(title, { opacity: 1 })
-
-      TweenMax.staggerFromTo(chars, 2, {
+      tl.set(title, { opacity: 1 })
+      tl.staggerFromTo(chars, 0.3, {
         opacity: 0,
         y: 10
       }, {
-        delay: 1,
         opacity: 1,
         y: 0,
         ease: Power4.easeOut
-      },0.1)
+      },0.03)
 
+      // animate scroll text in
+      tl.fromTo(subtitle, 0.3, {
+        y: 10
+      }, {
+        y: 0,
+        opacity: 1,
+        ease: Power4.easeOut
+      })
 
     }, 250)
 
