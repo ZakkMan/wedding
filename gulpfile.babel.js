ead7c9135f1261f4ef2fd9da1159be7fdb8da5af
Zakk Fleischmann <fleischmann@teamsubjectmatter.com>
2017-02-25 11:56:43 -0500

    update site for v2

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 /*
  *
  *  Table of Contents
@@ -17,53 +17,57 @@
  */
 
 // A. General
-import gulp         from 'gulp';
-import concat       from 'gulp-concat';
-import flatten      from 'gulp-flatten';
-import gulpif       from 'gulp-if';
-import lazypipe     from 'lazypipe';
-import merge        from 'merge-stream';
-import plumber      from 'gulp-plumber';
-import rev          from 'gulp-rev';
-import runSequence  from 'run-sequence';
-import buffer       from 'vinyl-buffer';
-import source       from 'vinyl-source-stream';
-import transform    from 'vinyl-transform';
-import sourcemaps   from 'gulp-sourcemaps';
-import del          from 'del';
-import fs           from 'fs';
-import rename       from 'gulp-rename';
-import uglify       from 'gulp-uglify';
-import changed      from 'gulp-changed';
+import gulp         from 'gulp'
+import concat       from 'gulp-concat'
+import flatten      from 'gulp-flatten'
+import gulpif       from 'gulp-if'
+import lazypipe     from 'lazypipe'
+import merge        from 'merge-stream'
+import plumber      from 'gulp-plumber'
+import rev          from 'gulp-rev'
+import runSequence  from 'run-sequence'
+import buffer       from 'vinyl-buffer'
+import source       from 'vinyl-source-stream'
+import transform    from 'vinyl-transform'
+import sourcemaps   from 'gulp-sourcemaps'
+import del          from 'del'
+import fs           from 'fs'
+import rename       from 'gulp-rename'
+import uglify       from 'gulp-uglify'
+import changed      from 'gulp-changed'
 
 // B. Scripts
-import babel        from "gulp-babel";
-import browserify   from 'browserify';
-import jshint       from 'gulp-jshint';
+import babel        from "gulp-babel"
+import browserify   from 'browserify'
+import jshint       from 'gulp-jshint'
 
 // C. Styles
-import sass         from 'gulp-sass';
-import cleanCSS     from 'gulp-clean-css';
-import autoprefixer from 'gulp-autoprefixer';
-import parker       from 'gulp-parker';
-import uncss        from 'gulp-uncss';
+import sass         from 'gulp-sass'
+import cleanCSS     from 'gulp-clean-css'
+import autoprefixer from 'gulp-autoprefixer'
+import parker       from 'gulp-parker'
+import uncss        from 'gulp-uncss'
 
-// D. Images
+// D. Templates
+import nunjucks from 'gulp-nunjucks-render'
+import data from 'gulp-data'
+
+// E. Images
 import imagemin     from 'gulp-imagemin'
 
-// E. Fonts
-import fontgen      from 'gulp-fontgen';
+// F. Fonts
+import fontgen      from 'gulp-fontgen'
 
-// F. Utilities
-import browserSync  from 'browser-sync';
-var reload = browserSync.reload;
+// G. Utilities
+import browserSync  from 'browser-sync'
+var reload = browserSync.reload
 
 /*
  *  2. General Tasks
  */
 gulp.task('clean', function() {
  del.sync('./public')
-});
+})
 
 
 /*
@@ -76,7 +80,7 @@ gulp.task('scripts', () => {
    transform: [
      ["babelify", {presets: ["es2015"]}]
    ]
- });
+ })
 
  return b
    .bundle()
@@ -86,12 +90,12 @@ gulp.task('scripts', () => {
    .pipe(uglify().on('error', function(err){ console.log(err) }))
    .pipe(rename('main.min.js'))
    .pipe(gulp.dest('./public/assets/scripts'))
-});
+})
 
 gulp.task('lint', () => {
   return gulp.src('./public/assets/scripts/main.js')
     .pipe(jshint())
-});
+})
 
 
 /*
@@ -119,26 +123,52 @@ gulp.task('styles', () => {
     }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./public/assets/styles'))
-    .pipe(reload({stream: true}));
+    .pipe(reload({stream: true}))
 })
 
 gulp.task('parker', function() {
   return gulp.src('./public/assets/main.css')
-    .pipe(parker());
-});
+    .pipe(parker())
+})
 
 
+/**
+ * Template Tasks
+ */
+gulp.task('templates', () => {
+
+  return gulp.src('src/templates/pages/*.nunjucks')
+    .pipe(plumber())
+    .pipe(data(getDataForFile))
+    .pipe(nunjucks({
+      path: 'src/templates'
+    }))
+    .pipe(gulp.dest('public'))
+
+})
+
+function getDataForFile( file ) {
+
+  let filename = file.relative.split('.')[0]
+  var pageData = JSON.parse(fs.readFileSync(`./src/data/${filename}.json`, 'utf8'))
+
+  return Object.assign({
+      pageType: filename
+    },
+    pageData
+  )
+
+}
 
 
 gulp.task('fonts', () => {
   return gulp.src('./src/assets/fonts/*')
-    // .pipe(flatten())
     .pipe(fontgen({
       dest: './public/assets/fonts'
     }))
     .pipe(gulp.dest('./public/assets/fonts'))
-    .pipe(browserSync.stream());
-});
+    .pipe(browserSync.stream())
+})
 
 gulp.task('images', () => {
   gulp.src('src/assets/images/*')
@@ -148,7 +178,7 @@ gulp.task('images', () => {
     }))
     .pipe(gulp.dest('public/assets/images'))
     .pipe(browserSync.stream())
-});
+})
 
 gulp.task('html', () => {
   gulp.src('./src/**/*.html')
@@ -156,8 +186,8 @@ gulp.task('html', () => {
 })
 
 gulp.task('default', () => {
-  gulp.start('build');
-});
+  gulp.start('build')
+})
 
 
 
@@ -165,20 +195,22 @@ gulp.task('default', () => {
  *  5. Watch
  */
  gulp.task('watch', () => {
+
    browserSync.init({
      notify: true,
      server: {
        baseDir: './public'
      }
-   });
+   })
 
-   let source = './src/assets';
-   gulp.watch(source + '/styles/**/*', ['styles']);
-   gulp.watch(source + '/scripts/**/*', ['scripts', reload]);
-   gulp.watch('./src/**/*.html', ['html', reload]);
-   gulp.watch(source + '/fonts/**/*', ['fonts']);
-   gulp.watch(source + '/images/**/*', ['images']);
- });
+   let source = 'src/assets'
+   gulp.watch(source + '/styles/**/*', ['styles'])
+   gulp.watch(source + '/scripts/**/*', ['scripts', reload])
+   gulp.watch('src/templates/**/*.nunjucks', ['templates', reload])
+   gulp.watch(source + '/fonts/**/*', ['fonts'])
+   gulp.watch(source + '/images/**/*', ['images'])
+
+ })
 
 
 /*
@@ -187,6 +219,6 @@ gulp.task('default', () => {
 gulp.task('build', ['clean'], () => {
  runSequence('styles',
              'scripts',
-             'html',
-             ['fonts', 'images']);
-});
+             'templates',
+             ['fonts', 'images'])
+})
